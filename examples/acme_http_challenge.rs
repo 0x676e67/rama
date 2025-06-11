27006e2bfde52e95a0c87d9d769840c10fdd6118
brecht stamper <stamper.brecht@gmail.com>
2025-06-11 20:57:21 +0200

    examples

---
+++ examples/acme_http_challenge.rs
@@ -0,0 +1,123 @@
+use rama::{
+    Context, Layer, graceful,
+    http::{
+        Body, HeaderValue,
+        layer::{compression::CompressionLayer, trace::TraceLayer},
+        server::HttpServer,
+        service::web::WebService,
+    },
+    rt::Executor,
+    tls::acme::{
+        Client,
+        proto::{
+            client::{CreateAccountOptions, KeyAuthorization, NewOrderPayload},
+            common::Identifier,
+            server::{ChallengeType, OrderStatus},
+        },
+    },
+};
+
+use std::{sync::Arc, time::Duration};
+use tokio::time::sleep;
+
+// Default directory url of pebble
+const TEST_DIRECTORY_URL: &str = "https://localhost:14000/dir";
+// Addr on which server will bind to do acme challenge
+const ADDR: &str = "0.0.0.0:5002";
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new(TEST_DIRECTORY_URL).await.unwrap();
+
+    let account = client
+        .create_account(CreateAccountOptions {
+            terms_of_service_agreed: Some(true),
+            contact: None,
+            external_account_binding: None,
+            only_return_existing: None,
+        })
+        .await
+        .unwrap();
+
+    let mut order = account
+        .new_order(NewOrderPayload {
+            identifiers: vec![Identifier::Dns("test.dev".into())],
+            ..Default::default()
+        })
+        .await
+        .unwrap();
+
+    let authz = order.get_authorizations().await.unwrap();
+
+    let auth = &authz[0];
+    let mut challenge = auth
+        .challenges
+        .iter()
+        .find(|challenge| challenge.r#type == ChallengeType::Http01)
+        .unwrap()
+        .to_owned();
+
+    let key_authz = order.create_key_authorization(&challenge);
+
+    let path = format!(".well-known/acme-challenge/{}", challenge.token);
+
+    println!("localhost:5002/{}", path);
+    tracing::info!("running service at: {ADDR}");
+
+    let state = Arc::new(ChallengeState {
+        key_authz: key_authz.clone(),
+    });
+
+    let graceful = graceful::Shutdown::default();
+
+    graceful.spawn_task_fn(|guard| async move {
+        let exec = Executor::graceful(guard.clone());
+        HttpServer::auto(exec)
+            .listen_with_state(
+                state,
+                ADDR,
+                (TraceLayer::new_for_http(), CompressionLayer::new()).layer(
+                    WebService::default().get(
+                        &path,
+                        |ctx: Context<Arc<ChallengeState>>| async move {
+                            println!("receving get request");
+                            let mut response = http::Response::new(Body::from(
+                                ctx.state().key_authz.as_str().to_owned(),
+                            ));
+                            let headers = response.headers_mut();
+                            headers.append(
+                                "content-type",
+                                HeaderValue::from_str("application/octet-stream").unwrap(),
+                            );
+                            response
+                        },
+                    ),
+                ),
+            )
+            .await
+            .unwrap();
+    });
+
+    sleep(Duration::from_millis(1000)).await;
+
+    order.notify_challenge_ready(&challenge).await.unwrap();
+
+    println!("waiting for challenge");
+    order
+        .poll_until_challenge_finished(&mut challenge, Duration::from_secs(30))
+        .await
+        .unwrap();
+
+    println!("waiting for order");
+    let state = order
+        .poll_until_all_authorizations_finished(Duration::from_secs(3))
+        .await
+        .unwrap();
+
+    assert_eq!(state.status, OrderStatus::Ready);
+}
+
+#[derive(Debug)]
+struct ChallengeState {
+    key_authz: KeyAuthorization,
+}
---
+++ examples/acme_tls_challenge_using_rustls.rs
@@ -0,0 +1,127 @@
+use rama::{
+    Context, Layer, graceful,
+    service::service_fn,
+    tcp::server::TcpListener,
+    tls::{
+        acme::{
+            Client,
+            proto::{
+                client::{CreateAccountOptions, NewOrderPayload},
+                common::Identifier,
+                server::OrderStatus,
+            },
+        },
+        rustls::{
+            dep::rustls::{
+                self,
+                server::{ClientHello as RustlsClientHello, ResolvesServerCert},
+                sign::CertifiedKey,
+            },
+            server::{TlsAcceptorData, TlsAcceptorLayer},
+        },
+    },
+};
+
+use std::{convert::Infallible, sync::Arc, time::Duration};
+use tokio::time::sleep;
+
+// Default directory url of pebble
+const TEST_DIRECTORY_URL: &str = "https://localhost:14000/dir";
+// Addr on which server will bind to do acme challenge
+const ADDR: &str = "0.0.0.0:5002";
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new(TEST_DIRECTORY_URL).await.unwrap();
+    let account = client
+        .create_account(CreateAccountOptions {
+            terms_of_service_agreed: Some(true),
+            contact: None,
+            external_account_binding: None,
+            only_return_existing: None,
+        })
+        .await
+        .unwrap();
+
+    let mut order = account
+        .new_order(NewOrderPayload {
+            identifiers: vec![Identifier::Dns("test.dev".into())],
+            ..Default::default()
+        })
+        .await
+        .unwrap();
+
+    let authz = order.get_authorizations().await.unwrap();
+    // println!("authz: {:?}", authz);
+    // println!("challenges: {:?}", authz[0].challenges);
+
+    let auth = &authz[0];
+
+    tracing::info!("running service at: {ADDR}");
+
+    let graceful = crate::graceful::Shutdown::default();
+
+    let (challenge, cert_key) = order.create_rustls_cert_for_acme_authz(auth).unwrap();
+    let mut challenge = challenge.to_owned();
+
+    let cert_resolver = Arc::new(ResolvesServerCertAcme::new(cert_key));
+
+    let mut server_config = rustls::ServerConfig::builder()
+        .with_no_client_auth()
+        .with_cert_resolver(cert_resolver);
+
+    server_config.alpn_protocols = vec![rama_net::tls::ApplicationProtocol::ACME_TLS.into()];
+
+    let acceptor_data = TlsAcceptorData::try_from(server_config).expect("create acceptor data");
+
+    graceful.spawn_task_fn(|guard| async move {
+        let tcp_service =
+            TlsAcceptorLayer::new(acceptor_data).layer(service_fn(internal_tcp_service_fn));
+
+        TcpListener::bind("127.0.0.1:5001")
+            .await
+            .expect("bind TCP Listener: tls")
+            .serve_graceful(guard, tcp_service)
+            .await;
+    });
+
+    sleep(Duration::from_millis(1000)).await;
+
+    order.notify_challenge_ready(&challenge).await.unwrap();
+
+    println!("waiting for challenge");
+    order
+        .poll_until_challenge_finished(&mut challenge, Duration::from_secs(30))
+        .await
+        .unwrap();
+
+    println!("waiting for order");
+    order
+        .poll_until_all_authorizations_finished(Duration::from_secs(3))
+        .await
+        .unwrap();
+
+    println!("new order state: {:?}", order.state());
+    assert_eq!(order.state().status, OrderStatus::Ready);
+}
+
+#[derive(Debug)]
+struct ResolvesServerCertAcme {
+    key: Arc<CertifiedKey>,
+}
+
+impl ResolvesServerCertAcme {
+    pub(crate) fn new(key: CertifiedKey) -> Self {
+        Self { key: Arc::new(key) }
+    }
+}
+
+impl ResolvesServerCert for ResolvesServerCertAcme {
+    fn resolve(&self, _client_hello: RustlsClientHello) -> Option<Arc<CertifiedKey>> {
+        return Some(self.key.clone());
+    }
+}
+
+async fn internal_tcp_service_fn<S>(_ctx: Context<()>, _stream: S) -> Result<(), Infallible> {
+    Ok(())
+}
